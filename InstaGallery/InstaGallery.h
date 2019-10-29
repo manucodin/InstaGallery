b9949b7404854bdb86403421758a3a6a00381049
Manu <manuel.rodriguez@wefish.app>
2019-10-29 21:13:38 +0100

    Initial Commit

---
+++ InstaGallery/InstaGallery.h
@@ -0,0 +1,19 @@
+//
+//  InstaGallery.h
+//  InstaGallery
+//
+//  Created by Manuel Rodríguez Sebastián on 29/10/2019.
+//  Copyright © 2019 MRodriguez. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstaGallery.
+FOUNDATION_EXPORT double InstaGalleryVersionNumber;
+
+//! Project version string for InstaGallery.
+FOUNDATION_EXPORT const unsigned char InstaGalleryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstaGallery/PublicHeader.h>
+
+
---
+++ InstaGalleryTests/InstaGalleryTests.swift
@@ -0,0 +1,34 @@
+//
+//  InstaGalleryTests.swift
+//  InstaGalleryTests
+//
+//  Created by Manuel Rodríguez Sebastián on 29/10/2019.
+//  Copyright © 2019 MRodriguez. All rights reserved.
+//
+
+import XCTest
+@testable import InstaGallery
+
+class InstaGalleryTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
