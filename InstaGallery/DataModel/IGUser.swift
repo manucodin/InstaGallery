d9cf08d803bdbbba199e42ee75b60b9f6ced7c51
Manu <manuel.rodriguez@wefish.app>
2020-02-26 15:40:07 +0100

    User url account

--- InstaGallery/DataModel/IGUser.swift
+++ InstaGallery/DataModel/IGUser.swift
@@ -8,19 +8,24 @@
 
 import Foundation
 
+private let urlAccountFormat = "https://www.instagram.com/%@"
+
 @objc public class IGUser :NSObject, Codable{
-    public var identifier          :String
-    public var account                :String
+    public var identifier           :String
+    public var account              :String
+    public var urlAccount           :String
     
     private enum CodingKeys :String, CodingKey{
-        case identifier     = "id"
-        case account           = "username"
+        case identifier         = "id"
+        case account            = "username"
     }
     
     required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         identifier = try container.decode(String.self, forKey: .identifier)
         account = try container.decode(String.self, forKey: .account)
+        
+        urlAccount = String(format: urlAccount, account)
     }
     
     public func encode(to encoder: Encoder) throws {}
